7484436bfebed9baea743fca94ebd0f4fdba105c
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-14 15:43:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,11 @@ def oxford_comma(array)
   if array.length == 1
     return array[0]
   else
-    last = array.pop
-    joined = array.join(", ")
     if array.length == 2
-      joined << " and #{last}"
+      joined = array.join(" and ")
     else
+      last = array.pop
+      joined = array.join(", ")
       joined << ", and #{last}"
     end
   end
